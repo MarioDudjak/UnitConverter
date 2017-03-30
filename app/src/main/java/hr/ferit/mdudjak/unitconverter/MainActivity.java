c2f274b22390bb3cf4d3e89294413e8d0f2738aa
Mario Dudjak <dudjakmario@hotmail.com>
2017-03-30 23:23:37 +0200

    First commit - Main layout

---
+++ app/src/main/java/hr/ferit/mdudjak/unitconverter/MainActivity.java
@@ -0,0 +1,16 @@
+package hr.ferit.mdudjak.unitconverter;
+
+import android.content.res.Configuration;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
