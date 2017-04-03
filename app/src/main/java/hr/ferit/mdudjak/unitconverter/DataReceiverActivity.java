8683f6b5d9d6fc2761c261d153909e3c6bacfd6a
Mario Dudjak <dudjakmario@hotmail.com>
2017-04-03 19:09:32 +0200

    Third commit - DataReceiverActivity + layout, round to 2 decimals, Calculate method for each unit Activity

---
+++ app/src/main/java/hr/ferit/mdudjak/unitconverter/DataReceiverActivity.java
@@ -0,0 +1,13 @@
+package hr.ferit.mdudjak.unitconverter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DataReceiverActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data_receiver);
+    }
+}
